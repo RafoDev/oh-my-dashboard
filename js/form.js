d9e7c47b5545224aab4556a0526473c2e5c776c8
jose2119 <josephpenafielloayza28@gmail.com>
2022-05-10 23:45:09 -0500

    update form.js and profile.html

--- js/form.js
+++ js/form.js
@@ -1,17 +1,10 @@
+const formulario = document.getElementById("formUser");
+debugger;
 const validacion = () => {
-  const form = document.getElementById("FormUser");
-  const input=document.querySelectorAll(".main-form__group-input");
-  form.addEventListener("submit", (e) => {
-      // const expNom = /[a-zA-Z ]/g;
-      // const usuario = input[0];
-      // const flgNom = expNom.test(nombre.value);
-      // const expCel = /^[0-9](?!.*00)\d{8}$/g;
-      // const cel = document.getElementById("txtCel");
-      // const flgCel = expCel.test(cel.value);
-      // const expCorreo = /^[A-Za-z0-9+_.-]+@(.+)$/g;
-      // const correo = input[1];
-      // const flgCorreo = expCorreo.test(correo.value);
-      // const mensaje = document.getElementById("txtArea");
+  console.log("hola mundo");
+  formulario.addEventListener("submit", (e) => {
+    const input=document.querySelectorAll(".main-form__group-input");
+    console.log(input);
       const usuario = input[0];
       const correo = input[1];
       const nombres=input[2];
@@ -35,54 +28,83 @@ const validacion = () => {
 
 
 
+//usuario
+if(flgUsuario==false){
+  e.preventDefault();
+  
+    Swal.fire({
+    icon: 'error',
+    title: 'El campo esta vacio',
+    text: 'Ingrese un nombre de usuario valido',
+    timer:2000
+  })}else{
+    alert("hola mundo");
+  };
+if(flgCorreo==false){
+  e.preventDefault();
+  
+    Swal.fire({
+    icon: 'error',
+    title: 'El campo esta vacio o el correo electronico es invalido',
+    text: 'Ingrese un correo electronico valido',
+    timer:2000
+  })};
+if(flgNombres==false){
+  e.preventDefault();
+  
+    Swal.fire({
+    icon: 'error',
+    title: 'El campo esta vacio o el Nombre contiene numeros o caracteres especiales',
+    text: 'Ingrese un Nombre Valido',
+    timer:2000
+  })};
+if(flgApellidos==false){
+  e.preventDefault();
+  
+    Swal.fire({
+    icon: 'error',
+    title: 'El campo esta vacio o el Apellido contiene numeros o caracteres especiales',
+    text: 'Ingrese un Apellido Valido',
+    timer:2000
+  })};
+if(flgDireccion==false){
+  e.preventDefault();
+  
+    Swal.fire({
+    icon: 'error',
+    title: 'El campo esta vacio o la direccion es invalida',
+    text: 'Ingrese una direccion correcta',
+    timer:2000
+  })};
 
-      //nombre
-      if (flgNom == false) {
-          e.preventDefault();
-          nombre.classList.add("contact__form-input--invalid");
-          document.getElementById("nombre").innerHTML =
-              "❌El campo esta vacio o tiene caracteres especiales y numeros,Ingrese un nombre correcto.";
-      } else if (flgNom == true) {
-          nombre.classList.remove("contact__form-input--invalid");
-          nombre.classList.add("contact__form-input--valid");
-          document.getElementById("nombre").innerHTML = "✅";
-      }
+  if(flgCiudad==false){
+    e.preventDefault();
+    
+      Swal.fire({
+      icon: 'error',
+      title: 'El campo esta vacio',
+      text: 'Ingrese la Ciudad',
+      timer:2000
+    })};
+  if(flgCodigo==false){
+    e.preventDefault();
+    
+      Swal.fire({
+      icon: 'error',
+      title: 'El campo esta vacio o el codigo de postal es invalido',
+      text: 'Ingrese un codigo de postal correcto',
+      timer:2000
+    })};
 
-      //celular
-      if (flgCel == false) {
-          e.preventDefault();
-          cel.classList.add("contact__form-input--invalid");
-          document.getElementById("celular").innerHTML =
-              "❌El campo esta vacio o tiene mas de 9 digitos,Ingrese un numero de celular correcto.";
-      } else if (flgCel == true) {
-          cel.classList.remove("contact__form-input--invalid");
-          cel.classList.add("contact__form-input--valid");
-          document.getElementById("celular").innerHTML = "✅";
-      }
 
-      //correo
-      if (flgCorreo == false) {
-          e.preventDefault();
-          correo.classList.add("contact__form-input--invalid");
-          document.getElementById("email").innerHTML =
-              "❌El campo esta vacio o no contiene el dominio de correo, Ingrese un correo electronico correcto.";
-      } else if (flgCorreo == true) {
-          correo.classList.remove("contact__form-input--invalid");
-          correo.classList.add("contact__form-input--valid");
-          document.getElementById("email").innerHTML = "✅";
-      }
-      //mensaje
-      if (mensaje.value.length == 0 || mensaje.value.length > 500) {
-          e.preventDefault();
-          mensaje.classList.add("contact__form-input--invalid");
-          document.getElementById("mensaje").innerHTML =
-              "❌El campo esta vacio o ha ingresado mas de 500 caracteres. Ingrese 500 caracteres como maximo.";
-      } else if (mensaje.value.length > 0 && mensaje.value.length <= 500) {
-          mensaje.classList.remove("contact__form-input--invalid");
-          mensaje.classList.add("contact__form-input--valid");
-          document.getElementById("mensaje").innerHTML = "✅";
-      }
+
+
+
+
+
+    
+      
   });
 };
 
-export default form;
+export default validacion;
--- js/index.js
+++ js/index.js
@@ -4,10 +4,12 @@ import stats from "../utils/stats.js";
 import charts from "./chart.js";
 import nav from "./nav.js";
 import { tasksStats, todo } from "./todo.js";
+import validacion from "./form.js";
 
 const documentReady = () => {
     nav();
     charts(stats);
     todo();
+    validacion();
 }
 document.addEventListener('DOMContentLoaded',documentReady);
